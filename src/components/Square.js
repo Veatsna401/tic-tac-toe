9f020518b175415fea08439b74f9f86e1c6ab679
Veez <veatsna@casetabs.com>
2020-06-09 23:43:01 -0700

    resolved console error

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const Square = ({value, onClick}) => {
-  console.log({value});
   return (
     <button className="square" onClick={onClick}>
-      <div className="face">
-        {value && <img src={value} width="130" height="130" />}
+      <div>
+        {value && <img src={value} alt="babies" width="130" height="130" />}
       </div>
     </button>
   );
